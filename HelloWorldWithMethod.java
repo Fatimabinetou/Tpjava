c5c910bc27e80a075cd13d3a03c33bda30068cd5
Fatoumata Binetou Diagne <fatoumatabinetoudiagne160@gmail.com>
2024-11-07 17:27:17 +0100

    Exercice 1

---
+++ HelloWorldWithMethod.java
@@ -0,0 +1,12 @@
+public class HelloWorldWithMethod
+ {
+   public void Hello()
+	{ 
+	  System.out.println("You're Welcome!");
+	}
+  
+  public static void main (String [] args)
+	{ HelloWorldWithMethod s=new HelloWorldWithMethod();
+	  s.Hello();
+	}
+}
